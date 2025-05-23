2da2ee023a00b806c30d614799b5f8a6163fdfb5
David Prasetyo <davidpras1994@gmail.com>
2025-05-23 10:04:04 +0700

    update genbat

--- gen.bat
+++ gen.bat
@@ -1,6 +1,6 @@
-@echo off
-echo Cleaning Flutter project...
-cmd /c flutter clean
+REM @echo off
+REM echo Cleaning Flutter project...
+REM cmd /c flutter clean
 
 echo Running Flutter pub get...
 cmd /c flutter pub get
@@ -9,7 +9,7 @@ echo Running Flutter pub run flutter_launcher_icons:main...
 cmd /c flutter pub run flutter_launcher_icons:main
 
 echo Running APK Build...
-cmd /c flutter build apk --release
+cmd /c flutter build apk --release --verbose
 
 REM Open the folder containing the APK
 start "" "build\app\outputs\apk\release"
